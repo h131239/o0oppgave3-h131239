8694b8c4934f84dfe6c706635c95a3d6140af231
Lars Kristensen <lmkr@hib.no>
2016-08-08 20:03:41 +0200

    Lagt inn basale metoder og tests

---
+++ src/no/hib/dat100/Kalkulator.java
@@ -0,0 +1,28 @@
+package no.hib.dat100;
+
+public class Kalkulator {
+
+	public static int add(int tall1, int tall2) {
+		return tall1 + tall2;
+	}
+
+	public static int sub(int tall1, int tall2) {
+		return tall1 - tall2;
+	}
+
+	public static int mul(int tall1, int tall2) {
+		return tall1 * tall2;
+	}
+
+	public static int div(int tall1, int tall2) {
+		return tall1 / tall2;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
