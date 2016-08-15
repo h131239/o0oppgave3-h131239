881329dec432b9de3a1944c7c3d01c027519b524
Lars Kristensen <lmkr@hib.no>
2016-08-15 11:43:38 +0200

    Klargjort for implementasjon av kalkulatormetoder

--- src/no/hib/dat100/Kalkulator.java
+++ src/no/hib/dat100/Kalkulator.java
@@ -3,26 +3,18 @@ package no.hib.dat100;
 public class Kalkulator {
 
 	public static int add(int tall1, int tall2) {
-		return tall1 + tall2;
+		return 0;
 	}
 
 	public static int sub(int tall1, int tall2) {
-		return tall1 - tall2;
+		return 0;
 	}
 
 	public static int mul(int tall1, int tall2) {
-		return tall1 * tall2;
+		return 0;
 	}
 
 	public static int div(int tall1, int tall2) {
-		return tall1 / tall2;
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		return 0;
 	}
 }
